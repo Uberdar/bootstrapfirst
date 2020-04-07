9d5f144ba463522bc33c6606271705f7e65ec8f3
Uberdar <allexvik@gmail.com>
2020-04-07 19:10:41 +0300

    ver 0.2

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function buttonClick() {
+ var v = document.getElementsByClassName("menu");
+ if (v[0].style.display === "inline-block") {
+ v[0].style.display = "none"
+ }else {
+	  v[0].style.display = "inline-block"
+ }
+}
